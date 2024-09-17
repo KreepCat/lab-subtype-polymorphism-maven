c7fdbe8fcb3976f2d6cb2e747165cc32c6ac85a8
AlexPollock <pollocka@grinnell.edu>
2024-09-17 15:11:58 -0500

    Finished Blocks

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # lab-subtype-polymorphism-maven
 
+Authors: Alex Pollock, Alyssa Ryan
+
 A lab on subtype polymorphism for Grinnell's CSC-207. This version of the lab uses Maven.
 
-This code may be found at <https://github.com/Grinnell-CSC207/lab-subtype-polymorphism-maven>.
+This code may be found at https://github.com/KreepCat/lab-subtype-polymorphism-maven 
--- src/main/java/edu/grinnell/csc207/experiments/Store.java
+++ src/main/java/edu/grinnell/csc207/experiments/Store.java
@@ -1,32 +1,32 @@
 package edu.grinnell.csc207.experiments;
 
-import edu.grinnell.csc207.store.Item;
+import java.io.PrintWriter;
 import edu.grinnell.csc207.store.BulkItem;
+import edu.grinnell.csc207.store.Item;
 import edu.grinnell.csc207.store.Purchase;
 import edu.grinnell.csc207.store.StoreUtils;
-
-import java.io.PrintWriter;
-
+import edu.grinnell.csc207.store.Multiple;
 /**
  * Some experiments with our store classes.
  *
  * @author Samuel A. Rebelsky
- * @author Your Name Here
+ * @author Alyssa Ryan and Alex Pollock
  */
 public class Store {
   /**
    * Run the experiments.
    *
-   * @param args
-   *   Command-line arguments.
+   * @param args Command-line arguments.
    */
   public static void main(String[] args) {
     PrintWriter pen = new PrintWriter(System.out, true);
     StoreUtils.printReceipt(pen,
         new Purchase[] {
-          new Item("hammer", "hits things", 899),
-          new Item("penny candy", "imaginary", 1),
-        });
+            new Item("hammer", "hits things", 899),
+            new Item("penny candy", "imaginary", 1),
+            new Item("will to live", "imaginary", 2000),
+            new BulkItem("pennies", "worth a cent", "penny", 2, 3),
+            new Multiple("nails", "to pound into wood", 2, 63)});
     pen.close();
   } // main(String[])
 } // class Store
